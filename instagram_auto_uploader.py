cb736e1750dca53f757cc8abde33dfd23cae2ac3
momomojo <35302851+momomojo@users.noreply.github.com>
2023-06-16 17:38:10 -0500

    Add files via upload

--- instagram_auto_uploader.py
+++ instagram_auto_uploader.py
@@ -1,9 +1,12 @@
 from instagram_api import InstagramAPI  # This would be the Instagram API library
+from google_api import fetch_data_from_google_sheets
 
 def post_to_instagram(unique_id, description, title, schedule, tags):
     # Use Instagram API to post video
     video_file = f"C:/Users/Momo Mojo/Desktop/youtube shorts/EmpoweringSage/{unique_id}.mp4"
-    InstagramAPI.post_video(video_file, title, description, tags)
+    # This is a two-step process: creating a container and then publishing it
+    container_id = InstagramAPI.create_container(video_file, title, description, tags)
+    InstagramAPI.publish_container(container_id)
 
 def main():
     # Fetch data from Google Sheets
